b9b3f4f2a683a47124d01d878558a5ec01047de8
Brad Jewell <brad@datafiniti.net>
2013-10-29 16:46:48 -0500

    trying to eliminate postgres oddities

--- lib/descartes/migrations/006_AddGraphDashboardRelationConstraints.rb
+++ lib/descartes/migrations/006_AddGraphDashboardRelationConstraints.rb
@@ -2,7 +2,7 @@
 Sequel.migration do
   up do
     alter_table(:graph_dashboard_relations) do
-      add_unique_constraint [:graph_id, :dashboard_id]
+      add_unique_constraint ['graphs.id', 'dashboard.id']
     end
   end
 end
